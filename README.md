7b29bcdd287bd669a969032b109bf16b39caa0db
peirios <mariafernandarios89@gmail.com>
2023-04-16 15:10:17 -0300

    Subida

---
+++ README.md
@@ -0,0 +1,9 @@
+# Práctica de Python
+
+## Consigna/enunciado
+
+Se genera un número secreto entero entre 1 y 100 que debe adivinar el jugador. Por cada número que dice el jugador el programa contesta diciendo si el número secreto es mayor o menor hasta que llegue a adivinarlo.
+
+## Ejecución
+
+Ingresá al [repl de este código](https://replit.com/@peirios/JuegoNumeroSecreto) para probar cómo funciona.
---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+# Con la función choice del módulo random, asigno un valor aleatorio a la variable «secret_number»
+secret_number = random.choice(range(1, 101))
+number = int(input("Ingresa un número entre 1 y 100: "))
+
+# El código itera las veces que sean necesarias hasta que adivine el número secreto.
+while number != secret_number:
+	if number > 100:
+		print("¡Error! Número fuera de rango")
+	elif number > secret_number:
+		print(f"El número secreto es menor que {number}")
+	else:
+		print(f"El número secreto es mayor que {number}")
+	number = int(input("\nElige otro número: "))
+	
+
+print(f"¡Felicidades! ¡Adivinaste! El número secreto es {secret_number}")
